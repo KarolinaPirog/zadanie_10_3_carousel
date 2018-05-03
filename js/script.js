01bc04650f84b9c7492e54682c983ad7aa50d117
Karolina Piróg <karolina.swieca@wp.pl>
2018-05-03 11:11:37 +0200

    change js and css

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-$(function(){
+$(function () {
     var carouselList = $('#carousel ul');
-    setInterval(changeSlide, 300);
-    carouselList.animate({'marginLeft':-450}, 500, moveFirstSlide);
+    setInterval(changeSlide, 3000);
 
+    function changeSlide() {
+        carouselList.animate({
+            'marginLeft': -525
+        }, 500, moveFirstSlide());
+    }
 
-function moveFirstSlide(){
-    var firstItem = carouselList.find('li:first');
-    var lastItem = carouselList.find('li:last');
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
+    function moveFirstSlide() {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({
+            marginLeft: 0
+        });
 
-}
+    }
 });
-moveFirstSlide();
